b5d622772e312ca9cac5ffccb5bd539e71e28fa3
Mateus Pacheco <mateuspacheco@alu.ufc.br>
2022-12-08 22:09:35 -0300

    Implement de JFileChooser

--- src/view/ExportFrame.java
+++ src/view/ExportFrame.java
@@ -3,19 +3,23 @@ package view;
 import control.SweepstakeDAO;
 
 import javax.swing.*;
+import java.io.File;
 
 public class ExportFrame {
     String fileName;
     public ExportFrame() {
 
-        fileName = JOptionPane.showInputDialog("Enter with the file name:", null);
-        SweepstakeDAO sweepstakeDAO = new SweepstakeDAO();
-        if(sweepstakeDAO.exportToFile(fileName)) {
-            JOptionPane.showMessageDialog(null, "Successfully wrote to the file!", "INFORMATION", JOptionPane.PLAIN_MESSAGE);
-        } else {
-            JOptionPane.showMessageDialog(null, "An error occurred!", "INFORMATION", JOptionPane.WARNING_MESSAGE);
-        }
+        JFileChooser fc = new JFileChooser();
 
+        if (fc.showDialog(null, "Export") == 0) {
+            File file = new File(fc.getSelectedFile().getPath()+".swpt");
+            SweepstakeDAO sweepstakeDAO = new SweepstakeDAO();
+            if(sweepstakeDAO.exportToFile(file)) {
+                JOptionPane.showMessageDialog(null, "Successfully wrote to the file!", "INFORMATION", JOptionPane.PLAIN_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "An error occurred!", "INFORMATION", JOptionPane.WARNING_MESSAGE);
+            }
+        }
         new MainMenu();
     }
 }
